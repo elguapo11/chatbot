89cdafca7092c2c22adee86cddccbffc2ccd40ca
Benny Mendoza <bennybc9387@gmail.com>
2023-08-27 17:12:33 -0600

    bones of chatbot created

---
+++ src/config.js
@@ -0,0 +1,9 @@
+import { createChatBotMessage } from 'react-chatbot-kit';
+
+const config = {
+  initialMessages: [
+    createChatBotMessage(`Hello world, this is Benny speaking`),
+  ],
+};
+
+export default config;
